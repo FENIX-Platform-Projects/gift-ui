7829346ad5a7f5104d6941ca5b5267564900e14a
Daniele Salvatore <daniele.salvatore@fao.org>
2016-12-06 11:04:40 +0100

    pre deploy

--- src/js/readyToUse/foodSafety.js
+++ src/js/readyToUse/foodSafety.js
@@ -159,7 +159,7 @@ define([
             columnPercentageItemLabel: label.toUpperCase(),
             cache: C.cache,
             environment: C.environment,
-            uid: "gift_process_total_food_consumption_" + this.model.uid,
+            uid: "gift_process_food_consumption_" + this.model.uid,
             selected_items: param.selected_items,
             selected_group: param.selected_group,
             process_name: param.process_name,
