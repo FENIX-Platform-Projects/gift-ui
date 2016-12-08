49250a0815d9b7476a14e8f534dbaae9bcec09c7
stefanocudini <stefano.cudini@gmail.com>
2016-12-08 15:49:31 +0100

    update map countries

--- src/json/consumption/countries_excluded.js
+++ src/json/consumption/countries_excluded.js
@@ -1,64 +1,195 @@
 define(function() { return [
-5,	//American Samoa,
-12,	//Argentina,
-17,	//Australia,
-18,	//Austria,
-21,	//Bahrain,
-27,	//Belgium,
-34,	//Bosnia and Herzegovina,
-35,	//Botswana,
-37,	//Brazil,
-40,	//Brunei Darussalam,
-41,	//Bulgaria,
-46,	//Canada,
-57,	//Colombia,
-65,	//Czech Republic,
-69,	//Denmark,
-73,	//Ecuador,
-78,	//Estonia,
-84,	//Finland,
-85,	//France,
-89,	//Gabon,
-93,	//Germany,
-97,	//Greece,
-113,	//Hungary,
-114,	//Iceland,
-117,	//Iran  (Islamic Republic of),
-119,	//Ireland,
-121,	//Israel,
-122,	//Italy,
-123,	//Jamaica,
-126,	//Japan,
-130,	//Jordan,
-137,	//Kuwait,
-140,	//Latvia,
-147,	//Lithuania,
-153,	//Malaysia,
-156,	//Malta,
-160,	//Mauritius,
-162,	//Mexico,
-167,	//Mongolia,
-177,	//Netherlands,
-179,	//New Zealand,
-186,	//Norway,
-195,	//Peru,
-198,	//Poland,
-199,	//Portugal,
-200,	//Puerto Rico,
-202,	//Republic of Korea,
-204,	//Russian Federation,
-220,	//Seychelles,
-223,	//Slovakia,
-224,	//Slovenia,
-227,	//South Africa,
-229,	//Spain,
-236,	//Sweden,
-240,	//Thailand,
-249,	//Turkey,
-255,	//United Arab Emirates,
-256,	//U.K. of Great Britain and Northern Ireland,
-259,	//United States of America,
-2648,	//Serbia,
-147295,	//China,
-147296	//Taiwan,
+2, //Aksai Chin
+3, //Albania
+4, //Algeria
+5, //American Samoa
+7, //Andorra
+8, //Angola
+9, //Anguilla
+10, //Antarctica
+11, //Antigua and Barbuda
+12, //Argentina
+14, //Aruba
+15, //Arunachal Pradesh
+16, //Ashmore and Cartier Islands
+17, //Australia
+18, //Austria
+19, //Azerbaijan
+20, //Bahamas
+21, //Bahrain
+22, //Baker Island
+24, //Barbados
+25, //Bassas da India
+26, //Belarus
+27, //Belgium
+28, //Belize
+30, //Bermuda
+32, //Bird Island
+34, //Bosnia and Herzegovina
+35, //Botswana
+36, //Bouvet Island
+37, //Brazil
+38, //British Indian Ocean Territory
+39, //British Virgin Islands
+40, //Brunei Darussalam
+41, //Bulgaria
+46, //Canada
+48, //Cayman Islands
+51, //Chile
+52, //China/India
+54, //Christmas Island
+55, //Clipperton Island
+56, //Cocos (Keeling) Islands
+57, //Colombia
+60, //Cook Islands
+61, //Costa Rica
+62, //Croatia
+63, //Cuba
+64, //Cyprus
+65, //Czech Republic
+69, //Denmark
+71, //Dominica
+72, //Dominican Republic
+73, //Ecuador
+76, //Equatorial Guinea
+78, //Estonia
+80, //Europa Island
+81, //Falkland Islands (Malvinas)
+82, //Faroe Islands
+83, //Fiji
+84, //Finland
+85, //France
+86, //French Guiana
+87, //French Polynesia
+88, //French Southern and Antarctic Territories
+89, //Gabon
+91, //Gaza Strip
+93, //Germany
+95, //Gibraltar
+96, //Glorioso Island
+97, //Greece
+98, //Greenland
+99, //Grenada
+100, //Guadeloupe
+101, //Guam
+102, //Abyei
+104, //Guernsey
+109, //Heard Island and McDonald Islands
+110, //Holy See
+112, //Howland Island
+113, //Hungary
+114, //Iceland
+117, //Iran  (Islamic Republic of)
+118, //Iraq
+119, //Ireland
+120, //Isle of Man
+121, //Israel
+122, //Italy
+123, //Jamaica
+126, //Japan
+127, //Jarvis Island
+128, //Jersey
+129, //Johnston Atoll
+130, //Jordan
+131, //Juan de Nova Island
+132, //Kazakhstan
+134, //Kingman Reef
+136, //Kuril islands
+137, //Kuwait
+140, //Latvia
+141, //Lebanon
+145, //Libya
+146, //Liechtenstein
+147, //Lithuania
+148, //Luxembourg
+149, //Macau
+151, //Madeira Islands
+153, //Malaysia
+154, //Maldives
+156, //Malta
+157, //Marshall Islands
+158, //Martinique
+160, //Mauritius
+161, //Mayotte
+162, //Mexico
+164, //Midway Island
+166, //Monaco
+168, //Montserrat
+172, //Namibia
+173, //Nauru
+174, //Navassa Island
+176, //Netherlands Antilles
+177, //Netherlands
+178, //New Caledonia
+179, //New Zealand
+183, //Niue
+184, //Norfolk Island
+185, //Northern Mariana Islands
+186, //Norway
+187, //Oman
+189, //Palau
+190, //Palmyra Atoll
+191, //Panama
+193, //Paracel Islands
+194, //Paraguay
+195, //Peru
+197, //Pitcairn
+198, //Poland
+199, //Portugal
+200, //Puerto Rico
+201, //Qatar
+202, //Republic of Korea
+203, //Romania
+204, //Russian Federation
+206, //Réunion
+207, //Saint Helena
+208, //Saint Kitts and Nevis
+209, //Saint Lucia
+210, //Saint Pierre et Miquelon
+211, //Saint Vincent and the Grenadines
+213, //San Marino
+215, //Saudi Arabia
+216, //Scarborough Reef
+218, //Senkaku Islands
+220, //Seychelles
+222, //Singapore
+223, //Slovakia
+224, //Slovenia
+227, //South Africa
+228, //South Georgia and the South Sandwich Islands
+229, //Spain
+230, //Spratly Islands
+233, //Suriname
+234, //Svalbard and Jan Mayen Islands
+236, //Sweden
+237, //Switzerland
+240, //Thailand
+241, //The former Yugoslav Republic of Macedonia
+244, //Tokelau
+245, //Tonga
+246, //Trinidad and Tobago
+247, //Tromelin Island
+248, //Tunisia
+249, //Turkey
+250, //Turkmenistan
+251, //Turks and Caicos islands
+252, //Tuvalu
+255, //United Arab Emirates
+256, //U.K. of Great Britain and Northern Ireland
+258, //United States Virgin Islands
+259, //United States of America
+260, //Uruguay
+263, //Venezuela
+265, //Wake Island
+266, //Wallis and Futuna
+268, //Western Sahara
+2647, //Montenegro
+33364, //Hong Kong
+40760, //Hala'ib triangle
+40762, //Ma'tan al-Sarra
+40781, //Jammu and Kashmir
+61013, //Ilemi triangle
+74578, //Azores Islands
+147295, //China
+147296 //Taiwan
 ]; });
