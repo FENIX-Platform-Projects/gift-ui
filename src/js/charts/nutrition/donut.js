3ef6871a5533a07b15417fc809b6364ae943a858
babic <Barbara.Cintoli@fao.org>
2017-03-07 16:36:47 +0100

    GIFT-6

--- src/js/charts/nutrition/donut.js
+++ src/js/charts/nutrition/donut.js
@@ -13,6 +13,7 @@ define([
         HEIGHT: 500,
         WIDTH: 500,
         level_number: 1,
+        total: "#pieThreeLevDrilldown-total",
         process: {
             first_level_process: [
                 {
@@ -262,6 +263,76 @@ define([
                         "value": "DESC"
                     }
                 }
+            ],
+            total_process:[
+                {
+                    "name": "gift_population_filter",
+                    "sid": [{"uid": "GIFT_afc_000042BUR201001"}],
+                    "parameters": {
+                        "gender": "2",
+                        "special_condition": ["2"],
+                        "age_year": {
+                            "from": 10.5,
+                            "to": 67
+                        }
+                        // "age_month": {
+                        //     "from": 10.5,
+                        //     "to": 67
+                        // }
+                    }
+                },
+
+                {
+                    "name" : "select",
+                    "parameters" : {
+                        "values" : {
+                            "item" : null,
+                            "group_code" : null,
+                            "subgroup_code" : null,
+                            "foodex2_code" : null,
+                            "value" : "value/<<raw_data_population_size[0]>>",
+                            "um" : null
+                        }
+                    }
+                },
+                {
+                    "name": "filter",
+                    "parameters": {
+                        "columns": [
+                            "group_code",
+                            "value",
+                            "um"
+                        ],
+                        "rows": {
+                            "item": {
+                                "codes": [
+                                    {
+                                        "uid": "GIFT_Items",
+                                        "codes": [
+                                            "IRON"
+                                        ]
+                                    }
+                                ]
+                            }
+                        }
+                    }
+                },
+
+                {
+                    "name": "group",
+                    "parameters": {
+                        "aggregations": [
+                            {
+                                "columns": ["value"],
+                                "rule": "SUM"
+                            },
+                            {
+                                "columns": ["um"],
+                                "rule": "max"
+                            }
+                        ]
+                    }
+                }
             ]
         }
     };
@@ -284,6 +355,12 @@ define([
             cache: this.cache
         });
         this._setHTMLvariables();
+
+        this._getProcessedResourceForChart(s.process.total_process)
+            .then(
+                _.bind(this._onTotalSuccess, this),
+                _.bind(this._onError, this)
+            );
 
         this._getProcessedResourceForChart(s.process.first_level_process)
             .then(
@@ -332,6 +409,18 @@ define([
         return this.bridge.getProcessedResource({body: process, params: {language: this.language}});
     };
 
+    ThreeLevDrilldown.prototype._onTotalSuccess = function (success) {
+
+        var r = success.data.length > 0 ? success.data[0] : [],
+            total = Formatter.format(r[0]) + " " + r[2];
+        console.log(total)
+        if (r[0]){
+            $(s.total).html(total);
+        } else {
+            $(s.total).html(" ");
+        }
+    };
+
     ThreeLevDrilldown.prototype._onSuccess = function (resource) {
         var series = this._processSeries(resource);
 
@@ -346,6 +435,59 @@ define([
         log.info("_onError");
         log.error(resource)
 
+    };
+
+    ThreeLevDrilldown.prototype._processTotalValueSeries = function (resource) {
+
+        var self = this;
+        var metadata = resource.metadata;
+        var data = resource.data;
+
+        var colors = RC["chartColors"];
+        var columns = metadata.dsd.columns;
+        var um_index = '', value_index = '', code_index = '', code_column_id, um_column_id;
+
+        for (var i = 0; i < columns.length; i++) {
+            if (columns[i].subject == 'um') {
+                um_index = i;
+                um_column_id = columns[i].id;
+            }
+            else if (columns[i].subject == 'value') {
+                value_index = i;
+            }
+            else if (columns[i].dataType == 'code') {
+                code_index = i;
+                code_column_id = columns[i].id;
+            }
+        }
+
+        var umLabelIdx = _.findIndex(columns, function (col) {
+            return col.id == um_column_id + '_' + self.language;
+        });
+
+        var codeLabelIdx = _.findIndex(columns, function (col) {
+            return col.id == code_column_id + '_' + self.language;
+        });
+
+        var dataToChart = [];
+
+        if (data) {
+            for (var i = 0; i < data.length; i++) {
+                var obj = {};
+                var it = data[i];
+
+                obj.y = Formatter.format(it[value_index]);
+                obj.unit = it[umLabelIdx];
+                obj.name = it[codeLabelIdx];
+                obj.code = it[code_index];
+                obj.drilldown = true;
+                obj.color = colors[obj.code];
+
+                dataToChart.push(obj);
+            }
+        }
+
+        return dataToChart;
     };
 
     ThreeLevDrilldown.prototype._processSeries = function (resource) {
